99bf5823f0c8ea90a0445bf4cb53d611781fa76e
N-Githu <u24607917@tuks.co.za>
2026-09-22 18:04:44 +0200

    Finished campus emergency coordinator

--- src/CampusEmergencyCoordinator.cpp
+++ src/CampusEmergencyCoordinator.cpp
@@ -1,6 +1,70 @@
 #include "CampusEmergencyCoordinator.h"
 
-void CampusEmergencyCoordinator::notify(Staff* sender, string event) {
-	// TODO - implement CampusEmergencyCoordinator::notify
-	throw "Not yet implemented";
+CampusEmergencyCoordinator(SecurityService security,
+							MedicalService medical,
+							FacilityService facility,
+							CommunicationService communication,)
+							{
+								securityService = security;
+								medicalService = medical;
+								facilityService = facility;
+								communicationService = communication;
+
+							}
+
+void CampusEmergencyCoordinator::notify(Staff* sender, const std::string& event){
+    if(event == "FireDetected")
+    {
+        medicalService->receiveUpdate(
+            "Prepare emergency response");
+
+        facilityService->receiveUpdate(
+            "Prepare evacuation");
+
+        communicationService->receiveUpdate(
+            "Issue fire alert");
+    }
+    else if(event == "MedicalEmergency")
+    {
+        facilityService->receiveUpdate(
+            "Clear emergency routes");
+
+        communicationService->receiveUpdate(
+            "Notify nearby responders");
+    }
+    else if(event == "IntruderDetected")
+    {
+        securityService->receiveUpdate(
+            "Dispatch additional guards");
+
+        communicationService->receiveUpdate(
+            "Issue security warning");
+    }
+    else if(event == "GasLeakDetected")
+    {
+        facilityService->receiveUpdate(
+            "Shut down affected systems");
+
+        securityService->receiveUpdate(
+            "Secure danger zone");
+
+        communicationService->receiveUpdate(
+            "Issue gas leak warning");
+    }
+    else if(event == "PowerFailure")
+    {
+        facilityService->receiveUpdate(
+            "Investigate outage");
+
+        communicationService->receiveUpdate(
+            "Notify affected buildings");
+    }
+    else if(event == "AllClear")
+    {
+        communicationService->receiveUpdate(
+            "Send all-clear notice");
+
+        facilityService->receiveUpdate(
+            "Resume normal operations");
+    }
 }
