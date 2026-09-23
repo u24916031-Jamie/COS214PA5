118287cb30771060d3b6665476e778e48347b7b2
N-Githu <u24607917@tuks.co.za>
2026-09-23 11:09:39 +0200

    Implemented CampusGaurdFacade

--- src/CampusGuardFacade.cpp
+++ src/CampusGuardFacade.cpp
@@ -1,11 +1,42 @@
 #include "CampusGuardFacade.h"
 
-void CampusGuardFacade::coordinateMassEvacuation() {
-	// TODO - implement CampusGuardFacade::coordinateMassEvacuation
-	throw "Not yet implemented";
+#include <iostream>
+
+CampusGuardFacade::CampusGuardFacade(CampusEmergencyCoordinator* coordinator, LockBuildingCommand* lockCommand,
+    UnlockBuildingCommand* unlockCommand, RestrictBuildingCommand* restrictCommand){
+
+    this->coordinator = coordinator;
+    this->lockCommand = lockCommand;
+    this->unlockCommand = unlockCommand;
+    this->restrictCommand = restrictCommand;
 }
 
-void CampusGuardFacade::resolveIncident() {
-	// TODO - implement CampusGuardFacade::resolveIncident
-	throw "Not yet implemented";
+void CampusGuardFacade::coordinateMassEvacuation(){
+    std::cout << "\n=== MASS EVACUATION INITIATED ===" << std::endl;
+
+    restrictCommand->execute();
+
+    coordinator->notify(nullptr, "FireDetected");
+}
+
+void CampusGuardFacade::coordinateBuildingLockdown(){
+    std::cout << "\n=== BUILDING LOCKDOWN INITIATED ===" << std::endl;
+
+    lockCommand->execute();
+
+    coordinator->notify( nullptr, "IntruderDetected");
+}
+
+void CampusGuardFacade::coordinateMedicalResponse(){
+    std::cout << "\n=== MEDICAL RESPONSE INITIATED ===" << std::endl;
+
+    coordinator->notify(nullptr, "MedicalEmergency");
+}
+
+void CampusGuardFacade::resolveIncident(){
+    std::cout << "\n=== INCIDENT RESOLVED ===" << std::endl;
+
+    unlockCommand->execute();
+
+    coordinator->notify(nullptr, "AllClear");
 }
--- src/CampusGuardFacade.h
+++ src/CampusGuardFacade.h
@@ -2,14 +2,27 @@
 #define CAMPUSGUARDFACADE_H
 
 #include "CampusEmergencyCoordinator.h"
+#include "LockBuildingCommand.h"
+#include "UnlockBuildingCommand.h"
+#include "RestrictBuildingCommand.h"
 
 class CampusGuardFacade {
 
 private:
 	CampusEmergencyCoordinator coordinator;
+	LockBuildingCommand* lockCommand;
+    UnlockBuildingCommand* unlockCommand;
+    RestrictBuildingCommand* restrictCommand;
 
 public:
+	CampusGuardFacade( CampusEmergencyCoordinator* coordinator, LockBuildingCommand* lockCommand,
+        UnlockBuildingCommand* unlockCommand, RestrictBuildingCommand* restrictCommand);
+		
 	void coordinateMassEvacuation();
+
+	void coordinateBuildingLockdown();
+
+	void coordinateMedicalResponse();
 
 	void resolveIncident();
 };
