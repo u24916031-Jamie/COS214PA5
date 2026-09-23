3cf4fc208107e4f67042abcf5555c682b5556d90
u25176502-commits <u25176502@tuks.co.za>
2026-09-23 19:36:28 +0200

    UML fixes

--- src/BuildingAccessInterface.cpp
+++ src/BuildingAccessInterface.cpp
@@ -1,4 +1,5 @@
 #include "BuildingAccessInterface.h"
+// #include iostream
 
 BuildingAccessInterface::BuildingAccessInterface(){
 	state = new UnlockedState();
@@ -11,4 +12,14 @@ BuildingAccessInterface::~BuildingAccessInterface(){
 void BuildingAccessInterface::setState(BuildingState* x){
 	delete state;
 	state = x;
+}
+
+bool BuildingAccessInterface::enter(char userPrefix){
+	bool allowed = state->allowAccess(userPrefix);
+
+	// if (allowed){
+	// 	cout << "You can enter\n";
+	// }
+	
+	return allowed;
 }
